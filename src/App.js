3fb15c00500889d577e7a56267c6cd391ee88bfc
cosmosgirlandcrows <infu242@gmail.com>
2022-03-11 13:53:28 +0100

    First attempt at component templates, all data is inputted in the App.js when the component is made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Button from './components/Button';
 import { useState, useEffect } from 'react';
 import './App.css';
+import dataElement from './components/DataElement';
+import {FaBeer} from 'react-icons/fa';
+import DataElement from './components/DataElement';
 
 function App() {
   const url = window.location.href;
@@ -9,7 +12,9 @@ function App() {
   const [fullname, setFullname] = useState(null);
   const [device, setDevice] = useState(null);
   const [batteryLevel, setBatteryLevel] = useState(null);
-  
+  const element1Icon = "<h2> <FaBeer></FaBeer> </h2>";
+  const [weight, getWeight] = useState(null);
+
   if (access_token == "") {
      window.location.href = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=23899M&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
   }
@@ -17,6 +22,7 @@ function App() {
   useEffect(() => {
     fetchProfile()
     fetchDevice()
+    fetchWeightTest()
   }, []) 
   
 
@@ -65,13 +71,41 @@ function App() {
     } );
   }
 
+  function fetchWeightTest(){
+
+    fetch('https://api.fitbit.com/1/user/' + userId + "/profile.json", {
+      headers: {
+        "authorization": "Bearer "+ access_token,
+        "accept": "application/json"
+      }
+    })
+    .then( response => response.json() )
+    .then( response => {
+      getWeight(response.user.weight);
+
+    });
+
+  }
+
   return (
     <>
       <Button onClick={logout} title="Logout"/>
       <h1>Profile : { fullname }</h1>
       <h2>Device : { device }</h2>
       <h2>Battery : { batteryLevel }</h2>
+
+
+      <DataElement
+        icon={element1Icon}
+        className={"dataElement"}
+        compTitle={"Test Element 1"}
+        textBodyMan={"Lorem ipsum dipsum jupp jupp"}
+        fetchData={weight}
+
+
+        />
     </>
+    
   );
 }
 
---
+++ src/components/DataElement.js
@@ -0,0 +1,22 @@
+//This code creates a template for the components we can use in app.js, letting us set the image and text for each manually through information fed into it
+
+const DataElement = ({icon, className, compTitle, textBodyMan, fetchData, fetchDataChart }) => {
+
+    return (
+
+        <div className={className}>
+            
+        <img src={icon}/>
+
+        <h3>{compTitle}</h3>
+        <p>{textBodyMan}</p>
+        <h3>{fetchData}</h3>
+
+        {/* Needs some imported component for data representation here, just treating it as a text object for now */}   
+            
+            </div>
+    )
+}
+
+
+export default DataElement
