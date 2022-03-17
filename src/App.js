91936a754056ab0e7c7cf56c69ae6b45e78f5973
Nojus <s344224@oslomet.no>
2022-03-17 17:26:44 +0100

    fetch api updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import Button from "./components/Button";
 import { useState, useEffect } from "react";
 import "./App.css";
 import useFetch from "./hooks/useFetch";
+import Header from "./components/Header";
+import { AuthenticationContext } from "./contexts/AuthenticationContext";
 
 function App() {
   const url = window.location.href;
   const access_token = getAccessToken(url);
   const userId = getUserId(url);
-  const [fullname, setFullname] = useState(null);
-  const [device, setDevice] = useState(null);
-  const [batteryLevel, setBatteryLevel] = useState(null);
-  const { data: profile, get: getProfile } = useFetch(access_token);
-  const { data: deviceInfo, get: getDeviceInfo } = useFetch(access_token);
 
   if (access_token == "") {
     window.location.href =
@@ -19,53 +16,13 @@ function App() {
   }
 
   //Fetch data here
-  useEffect(() => {
-    getProfile("https://api.fitbit.com/1/user/" + userId + "/profile.json");
-    getDeviceInfo("https://api.fitbit.com/1/user/" + userId + "/devices.json");
-  }, []);
-
-  function logout() {
-    const params = "token=" + access_token;
-    fetch("https://api.fitbit.com/oauth2/revoke", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic MjM4OTlNOmEwNmY1N2ZkY2RiMjgzMDc4ZDNjODBhNGY4YTE0NTVj",
-      },
-      body: params,
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        window.location.href = "http://localhost:3000/";
-      });
-  }
-
-  //GET call to fetch device info
-  function fetchDevice() {
-    fetch("https://api.fitbit.com/1/user/" + userId + "/devices.json", {
-      headers: {
-        authorization: "Bearer " + access_token,
-        accept: "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        setDevice(response[0].deviceVersion);
-        setBatteryLevel(response[0].batteryLevel + "%");
-      });
-  }
+  useEffect(() => {}, []);
 
   return (
     <>
-      <Button onClick={logout} title="Logout" />
-      {profile && <h1>Profile : {profile.user.fullName}</h1>}
-      {deviceInfo && (
-        <>
-          <h2>Device : {deviceInfo[0].deviceVersion}</h2>
-          <h2>Battery : {deviceInfo[0].batteryLevel + "%"}</h2>
-        </>
-      )}
+      <AuthenticationContext.Provider value={{ userId, access_token }}>
+        <Header />
+      </AuthenticationContext.Provider>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetch from "../hooks/useFetch";
+import Button from "./Button";
+import { useContext } from "react";
+import { AuthenticationContext } from "../contexts/AuthenticationContext";
+import { BsSmartwatch } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { access_token, userId } = useContext(AuthenticationContext);
+  const { logout } = useFetch(access_token);
+  const { data: profile, get: getProfile } = useFetch(access_token);
+  const { data: deviceInfo, get: getDeviceInfo } = useFetch(access_token);
+
+  useEffect(() => {
+    getProfile("https://api.fitbit.com/1/user/" + userId + "/profile.json");
+    getDeviceInfo("https://api.fitbit.com/1/user/" + userId + "/devices.json");
+  }, []);
+
+  return (
+    <header className="mainHeader">
+      <h1>Fitbit API</h1>
+      <div className="profile">
+        {profile && <img src={profile.user.avatar} alt="avatar" />}
+        <div>
+          {profile && <h2>{profile.user.fullName}</h2>}
+          {deviceInfo && (
+            <div className="deviceInfo">
+              <h3>
+                <BsSmartwatch />
+                Fitbit {deviceInfo[0].deviceVersion}
+              </h3>
+            </div>
+          )}
+        </div>
+        <Button onClick={() => logout()} title="Logout" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
