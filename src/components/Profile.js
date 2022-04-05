6d879ba29dd7193e6a8e0a6ac64458e07d9bb175
Nojus133 <budreikanojus@gmail.com>
2022-04-05 12:31:12 +0200

    Update Profile.js
    App doesn't crash now if device info could not be fetched

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -32,13 +32,13 @@ const Profile = ({ access_token, apiProfile, apiDeviceInfo }) => {
       {profile && <img src={profile.user.avatar} alt="avatar" />}
       <div>
         {profile && <h2>{profile.user.fullName}</h2>}
-        {/* <h3 className="deviceInfo">
-          {device && (
+        <h3 className="deviceInfo">
+          {device && (device[0]?.deviceVersion &&
             <>
               <BsSmartwatch /> Fitbit {device[0].deviceVersion}
             </>
           )}
-        </h3> */}
+        </h3>
       </div>
     </>
   );
