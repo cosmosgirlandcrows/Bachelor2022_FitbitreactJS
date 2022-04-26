1a9c3247c7c5439df24330be2cb8095bbff908ca
Nojus <s344224@oslomet.no>
2022-04-26 22:13:19 +0200

    Calendar implementation

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,6 +4,10 @@ import HeartrateComponent from "./HeartrateComponent";
 import SleepComponent from "./SleepComponent";
 import StepsComponent from "./StepsComponent";
 import CaloriesComponent from "./CaloriesComponent";
+import Calendar from "react-calendar";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+// import "react-calendar/dist/Calendar.css";
 
 const Grid = () => {
   const { userId, BASE_URL } = useContext(AuthenticationContext);
@@ -13,12 +17,35 @@ const Grid = () => {
   const apiSteps = `${BASE_URL}${userId}/activities/steps/date/`;
   const apiCalories = `${BASE_URL}${userId}/activities/calories/date/`;
 
+  const findWeekendDays = ({ activeStartDate, date, view }) =>
+    view === "month" && (date.getDay() === 0 || date.getDay() === 6)
+      ? "tile weekend"
+      : "tile";
+
+  const minDate = () => {
+    const d = new Date();
+    d.setFullYear(d.getFullYear() - 1);
+    return d;
+  };
+
   return (
     <div className="container">
       <HeartrateComponent base_url={apiHeartrate} />
       <SleepComponent base_url={apiSleep} />
       <StepsComponent base_url={apiSteps} />
       <CaloriesComponent base_url={apiCalories} />
+      <Calendar
+        className="calendar"
+        tileClassName={findWeekendDays}
+        nextLabel={<MdKeyboardArrowRight className="calendarArrowIcon" />}
+        next2Label={null}
+        prevLabel={<MdKeyboardArrowLeft className="calendarArrowIcon" />}
+        prev2Label={null}
+        showNeighboringMonth={false}
+        minDetail="year"
+        minDate={minDate()}
+        maxDate={new Date()}
+      />
     </div>
   );
 };
